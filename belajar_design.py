fda5388420217537faae7f854cff1196e20ea574
kickymaulana <kickymaulana@gmail.com>
2026-06-28 14:49:10 +0700

    ulangin kode

--- belajar_design.py
+++ belajar_design.py
@@ -4,39 +4,25 @@ from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import Slot
 from random import choice
 
-class AplikasiKita: # Tidak perlu inherit QMainWindow di sini karena kita pakai loader direktori
+
+class AplikasiKita:
     def __init__(self) -> None:
         loader: QUiLoader = QUiLoader()
-        
-        # Memuat UI. Jendela asli Qt Designer dimuat langsung ke self.ui
         self.ui: QMainWindow = loader.load("latihan.ui")
-        
-        self.inisialisasi_interface()
-
-    def inisialisasi_interface(self) -> None:
         self.ui.setWindowTitle("Aplikasi PySide6 Pertama")
-
-        # Akses komponen langsung dari objek self.ui
+        self.inisialisasi_interface()
+    def inisialisasi_interface(self) -> None:
         self.btn_halo: QPushButton = self.ui.btn_halo
         self.label_utama: QLabel = self.ui.label_utama
-
         self.btn_halo.clicked.connect(self.saat_tombol_ditekan)
-
     @Slot()
     def saat_tombol_ditekan(self) -> None:
-        nama: list[str] = ["amba", "arief", "arfy", "nongpal", "namad", "mochie", "vinz"]
+        nama: list[str] = ["kicky", "yildi", "dedi", "ricky"]
         self.label_utama.setText(choice(nama))
-        print("label diganti!")
-
-
+        print("label diganti")
 if __name__ == "__main__":
     aplikasi: QApplication = QApplication(sys.argv)
-    
-    # Inisialisasi class
-    main_app: AplikasiKita = AplikasiKita()
-    
-    # Tampilkan jendela ui-nya langsung
-    main_app.ui.show()
+    jendela: AplikasiKita = AplikasiKita()
+    jendela.ui.show()
 
-    # sys.exit memastikan terminal mendapat sinyal exit code (0) saat windows ditutup
     sys.exit(aplikasi.exec())
