5fa48c1d2474c978239f38ea7c64fb4e16094137
kickymaulana <kickymaulana@gmail.com>
2026-06-28 15:16:45 +0700

    belajar qline_edit.py

--- qline_edit.py
+++ qline_edit.py
@@ -1,24 +1,28 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QLineEdit
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import Slot
-from random import choice
 
 class AplikasiKita:
     def __init__(self) -> None:
         loader: QUiLoader = QUiLoader()
         self.ui: QMainWindow = loader.load("latihan.ui")
-        self.ui.setWindowTitle("Aplikasi PySide6 Pertama")
+        self.ui.setWindowTitle("Belajar input PySide6")
         self.inisialisasi_interface()
     def inisialisasi_interface(self) -> None:
         self.btn_halo: QPushButton = self.ui.btn_halo
         self.label_utama: QLabel = self.ui.label_utama
+        self.input_nama: QLineEdit = self.ui.input_nama
         self.btn_halo.clicked.connect(self.saat_tombol_ditekan)
     @Slot()
     def saat_tombol_ditekan(self) -> None:
-        nama: list[str] = ["Kicky", "Dedi", "Yildiz", "Ricky"]
-        self.label_utama.setText(choice(nama))
-        print("label diganti")
+        nama_inputan: str = self.input_nama.text()
+        if nama_inputan.strip() == "":
+            self.label_utama.setText("Tolong isi dulu namanya bos")
+        else:
+            self.label_utama.setText(f"Halo, Selamat datang {nama_inputan}")
+            print(f"User menginput {nama_inputan}")
+
 if __name__ == "__main__":
     aplikasi: QApplication = QApplication(sys.argv)
     jendela: AplikasiKita = AplikasiKita()
